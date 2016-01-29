ef79734a87557302ca75b8ef400fc04b3858a6bd
Chris Metcalf <chris@studiotenbelow.com>
2016-01-29 15:28:21 +0000

    Created contact form view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129122533) do
+ActiveRecord::Schema.define(version: 20160129151125) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
